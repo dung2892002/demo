78c7c34360c0e6a8dac0391ab49383d4f03eb487
Lê Dũng <vandung2002ts@gmail.com>
2025-02-21 14:20:35 +0700

    Update .gitignore and remove ignored files

--- Back-End/Cukcuk.Api/obj/Debug/net8.0/Cukcuk.Api.AssemblyInfo.cs
+++ Back-End/Cukcuk.Api/obj/Debug/net8.0/Cukcuk.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cukcuk.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e80d724233b0169dbf9776eda21b5adb9e99b1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c9e802098deffe68b77b063f3dae55ca73fc318")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cukcuk.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cukcuk.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Back-End/Cukcuk.Core/obj/Debug/net8.0/Cukcuk.Core.AssemblyInfo.cs
+++ Back-End/Cukcuk.Core/obj/Debug/net8.0/Cukcuk.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cukcuk.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e80d724233b0169dbf9776eda21b5adb9e99b1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c9e802098deffe68b77b063f3dae55ca73fc318")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cukcuk.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cukcuk.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
